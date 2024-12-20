08ba38b04d81db1768e0b82b136f2a7f4c0b3e5a
tktart <tonkow235@gmail.com>
2024-12-20 17:48:57 +0700

    u

--- lab10_2.cpp
+++ lab10_2.cpp
@@ -4,15 +4,30 @@
 using namespace std;
 
 int main (){
-	ifstream source;
-	ofstream dest;
-	source.open("cheerbook.txt") ;
-	dest.open("cheerbook_copy.txt");
-	
-	//กรอก Code เฉพาะส่วนนี้ ให้สอดคล้องกับโจทย์และ Code ส่วนอื่น ๆ 
-	//โดยห้ามแก้ไข Code ในส่วนอื่น ๆ
-
+    ifstream source;
+    ofstream dest;
+    source.open("cheerbook.txt");
+    dest.open("cheerbook_copy.txt");
+    
+    // กรอก Code เฉพาะส่วนนี้ ให้สอดคล้องกับโจทย์และ Code ส่วนอื่น ๆ 
+    if (!source) {
+        cerr << "ไม่สามารถเปิดไฟล์ cheerbook.txt ได้" << endl;
+        return 1;
+    }
+    
+    // เขียนข้อความในบรรทัดแรก
+    dest << "-------------------- BOOM ---------------------\n";
+    
+    // คัดลอกข้อมูลจาก cheerbook.txt ไปยัง cheerbook_copy.txt
+    string line;
+    while (getline(source, line)) {
+        dest << line << endl;
+    }
+    
+    // เขียนข้อความในบรรทัดสุดท้าย
+    dest << "-------------------- HA!! ---------------------\n";
+    
     source.close();
     dest.close();
-	return 0;
+    return 0;
 }
